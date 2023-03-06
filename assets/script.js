326abdde250fb2758a86094a594d486c33fd301d
kaleikautakaoka <107520051+kaleikautakaoka@users.noreply.github.com>
2023-03-06 14:18:24 -0700

    fix window promt to window alert and target generate button

--- assets/script.js
+++ assets/script.js
@@ -13,27 +13,22 @@ const lowerCase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', '
 const upperCase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 const varscombined = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '?', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
 
+//Function to create a password
+function passwordSelection() {
+var selectLength = window.alert("Please select a password length between 15-110")
+var passwordLength = parseInt(selectLength)
+}
 
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
 function writePassword() {
-  var addNumbers = window.confirm("Add numbers?")
-  var password = generatePassword();
+  var pwd = passwordSelection(15, 110);
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = pwd;
 
-  if (addNumbers === true) {
-    allVars.push(numbers)
-  }
-
-  return
 }
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
