d7ef23753943050356bd299ccee3227a60de2715
kaleikautakaoka <107520051+kaleikautakaoka@users.noreply.github.com>
2023-03-08 13:34:41 -0700

    readme and list function

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Uncrackable
-A password generator that provides uncrackable passwords!
+## Table of Contents
+
+- [Description](#Description)
+- [Project URL](#Project-URL)
+- [Deployment](#Deployment)
+- [Demo/Screenshots](#Demo/Screenshots)
+- [Technologies](#Technologies)
+- [License](#License)
+- [Resources](#Resources)
+- [Contact](#Contact)
+- [Author](#Author)
+
+## Project URL
+
+<>
+
+## Delpoyment
+
+- []()
+
+## Demo/Screenshots
+
+  <table>
+    <tr>
+      <td>Uncrackable Page Example</td>
+    </tr>
+    <tr>
+      <td><img src="./assets/images/passwordgen.png" height=300px alt="screenshot of Uncrackable webpage"></td>
+    </tr>
+  </table>
+
+## Technologies
+
+```
+HTML, CSS, Javascript
+```
+
+## License
+
+This project is not licensed
+
+## Resources
+- [codecadmey](codecadmey.com/)
+- [youtube](https://youtube.com/)
+- [w3schools](https://www.w3schools.com/)
+-[javascript.info](https://javascript.info/structure)
+-[GURU99](https://www.guru99.com/interactive-javascript-tutorials.html)
+-[freecodecamp](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/)
+
+## Contact
+
+Email: sasasa@gmail.com
+
+## Author
+
+Author(s): Sachi Kaleikau-Takaoka
+GitHub: <https://github.com/kaleikautakaoka>
--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,8 @@ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '?'];
 var lowerCase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 var upperCase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-var varsEmpty = []
+var varsEmpty = [];
+var varCombined = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '?', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 
 //Function to create password with prompt for selection length and parseInt to convert the string into a number
 function passwordSelection() {
@@ -11,27 +12,85 @@ var selectLength = window.prompt("Please select a password length between 15-90"
 var passwordLength = parseInt(selectLength);
 //if isNan means if its not a number it will prompt user to add a number
 if (isNaN(passwordLength)) {
-  window.alert("Please select a NUMBER between 15-90")
+  var selectLength = window.prompt("Please select a NUMBER between 15-90")
+  var passwordLength = parseInt(selectLength);
   return
 }
 
 //If statement to ensure a number is selected between 15-90 otherwise an alert will appear
 if (passwordLength < 15 || passwordLength > 90) {
-    window.alert("Invalid Selection. Please choose a number between 15-90")
+    window.prompt("Invalid Selection. Please choose a NUMBER BETWEEN 15 and 90")
+    var passwordLength = parseInt(selectLength);
+    return
 }
 //window.confirm box to confirm selection without text area
-if (passwordLength > 15 || passwordLength < 90) {
+if (passwordLength >= 15 || passwordLength <= 90) {
  var addSymbols = window.confirm("Include Symbols?")
  var addNumbers = window.confirm("Include Numbers?")
  var addUpperC = window.confirm("Include Upper Case Letters?")
  var addLowerC = window.confirm("Include Lower Case Letters")
+ return
 }
 
+//If statements execute different options
 if (addSymbols === true) {
 varsEmpty.push(symbols)
-return
 }
 
+if (addSymbols !== true) {
+  numbers.push(upperCase)
+}
+
+if (addNumbers == true) {
+  varsEmpty.push(numbers)
+}
+
+if (addNumbers !== true) {
+  symbols.push(upperCase)
+}
+
+if (addUpperC == true) {
+    varsEmpty.push(upperCase)
+}
+
+if (addUpperC !== true) {
+  numbers.push(lowerCase)
+}
+
+if (addLowerC == true) {
+      varsEmpty.push(lowerCase)
+}
+  
+  if (addLowerC !== true) {
+        numbers.push(upperCase)
+}
+
+if (addLowerC && addUpperC && addNumbers && addSymbols == true) {
+        varCombined.push(upperCase, lowerCase, numbers, symbols)
+}
+var generatedPwd = ""
+//function for random number
+function randomNumber(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rdm = Math.random();
+  return Math.floor(min*(1-rdm) + rdm*max)
+}
+
+//Function for the for loop
+function randomNum(list) {
+  return list[randomNum(list.length)]
+}
+
+//For loop to run through options and get random list items
+for (var i = 0; i < passwordLength; i++) {
+  var randomList = randomNum(varCombined);
+  var randomSymbol = randomNum(randomEverything);
+  generatedPwd += randomEverything
+}
+return generatedPwd
 }
 
 var generateBtn = document.querySelector("#generate");
